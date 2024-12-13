6101e162c1e75e56b616127d8e0b9ac8a84176df
VaishnaviMichiganTech <vhbalamb@mtu.edu>
2024-12-13 09:56:54 -0500

    Adding source files and OBU configuration files

---
+++ build/gps_nmea_sender_2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/moji/gps_workspace/src/gps_nmea_sender_2
+# Build directory: /home/moji/gps_workspace/build/gps_nmea_sender_2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
